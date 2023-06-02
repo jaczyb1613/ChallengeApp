df3871531de0a49d79411d196d013ba4be1077b5
Jacek Zybaczynski <jaczyb1613@gmail.com>
2023-06-02 22:05:41 +0100

    Dzień 4 1/2 zadania z tego dnia wykonania Moje imie i naziwsko, wiek, oraz kraj

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Hello, What is your name?!");
-Console.WriteLine("Hello, Jacek!");
+﻿string name = "Jacek ";
+string surname = "Zybaczynski";
+String question = "Where Are You from?";
+String answer = " Poland";
+String result = name + surname;
+String result2 = question + answer;
+int myAge = 33;
+Console.WriteLine("Full Name: " + result);
+Console.WriteLine("Age: " + myAge);
+Console.WriteLine(result2);
+
