ea21bb06f3c685890f412aeb01629e7cff71aef6
Jacek Zybaczynski <jaczyb1613@gmail.com>
2023-07-17 21:16:09 +0100

    Zadanie dzień 5  17/7/2023

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.Design;
 
-int number = 45662348;
-string numberAsString = number.ToString();
-char[]letters = numberAsString.ToArray();
+int number = 1003439722;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+
 int counter0 = 0;
 int counter1 = 0;
 int counter2 = 0;
@@ -14,51 +15,49 @@ int counter7 = 0;
 int counter8 = 0;
 int counter9 = 0;
 
-foreach (char letter in letters)
+foreach(char letter in letters)
 {
     if(letter == '0')
-    { 
-       counter0++;
+    {
+        counter0++;
     }
-    else if (letter == '1')
-     { 
-        counter1++; 
-     } 
-    else if (letter == '2') 
+    else if(letter == '1') 
     { 
+        counter1++;
+    }
+    else if (letter == '2')
+    {
         counter2++;
     }
-    else if (letter == '3') 
-    { 
+    else if (letter == '3')
+    {
         counter3++;
     }
-    else if(letter == '4')
+    else if (letter == '4')
     {
         counter4++;
     }
-    else if(letter == '5')
+    else if (letter == '5')
     {
         counter5++;
     }
-    else if(letter == '6')
+    else if (letter == '6')
     {
         counter6++;
     }
-    else if(letter == '7')
+    else if (letter == '7')
     {
         counter7++;
     }
-    else if(letter == '8')
+    else if (letter == '8')
     {
         counter8++;
     }
-    else if(letter == '9')
+    else if (letter == '9')
     {
         counter9++;
     }
-    
 }
-
 Console.WriteLine("Wynik dla liczby: " + number);
 Console.WriteLine("0 = " + counter0);
 Console.WriteLine("1 = " + counter1);
